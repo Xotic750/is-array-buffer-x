e32f9d39b5ca110d99b357750afb8ca7556c30ba
Graham Fairweather <xotic750@gmail.com>
2019-07-16 22:44:31 +0200

    Terraformed
    Auto build

--- __tests__/is-array-buffer-x.test.js
+++ __tests__/is-array-buffer-x.test.js
@@ -3,29 +3,6 @@
 
 let isArrayBuffer;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  isArrayBuffer = require('../../index.js');
-} else {
-  isArrayBuffer = returnExports;
-}
-
 const ifHasArrayBuffer = typeof ArrayBuffer === 'function' ? it : xit;
 
 describe('isArrayBuffer', function() {
