38022419a171d0330d4f9d5f37bc0c8d3f1fbb29
Graham Fairweather <xotic750@gmail.com>
2019-07-22 01:18:27 +0200

    :bookmark: v2.0.5
    Auto build

--- dist/is-array-buffer-x.js
+++ dist/is-array-buffer-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-19T22:29:53.843Z",
+  "date": "2019-07-21T23:18:21.461Z",
   "describe": "",
   "description": "Detect whether or not an object is an ArrayBuffer.",
   "file": "is-array-buffer-x.js",
   "hash": "34b5742bd49fd07cb81d",
   "license": "MIT",
-  "version": "2.0.4"
+  "version": "2.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
