47441194151709d0d8f752c8c755f3a89a6624df
Graham Fairweather <xotic750@gmail.com>
2020-01-31 01:21:02 +0100

    Maintenance

--- dist/is-array-buffer-x.js
+++ dist/is-array-buffer-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-09-05T20:17:56.946Z",
+  "date": "2020-01-31T00:20:54.486Z",
   "describe": "",
   "description": "Detect whether or not an object is an ArrayBuffer.",
   "file": "is-array-buffer-x.js",
